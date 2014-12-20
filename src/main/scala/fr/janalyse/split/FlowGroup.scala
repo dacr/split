6ef685ccd6626b12d8b6eb06dd5024f21cdcf779
crosson.david@gmail.com <crosson.david@gmail.com>
2014-12-20 15:35:44 +0100

    scaladoc

--- src/main/scala/fr/janalyse/split/FlowGroup.scala
+++ src/main/scala/fr/janalyse/split/FlowGroup.scala
@@ -6,11 +6,21 @@ object FlowGroup {
 
   /**
    * collection of strings into a shorter one,
-   * a regular expression is used as the separator
+   * @in the flow of strings to process
+   * @param startRE a regular expression is used as the separator
+   * @param mks string separator to use to groups strings together
+   * @return the new flow of stream
    */
   def restrings(in: Stream[String], startRE: Regex,mks:String): Stream[String] =
     restrings(in, startRE.findFirstIn(_).isDefined,mks)
 
+  /**
+   * collection of strings into a shorter one
+   * @in the flow of strings to process
+   * @param startTest test function to know if the given string marks the beginning of a new string block
+   * @param mks string separator to use to groups strings together
+   * @return the new flow of stream
+   */
   def restrings(in: Stream[String], startTest: String => Boolean, mks:String): Stream[String] = {
     def build(first: String, nexts: List[String]) = (first :: nexts).mkString(mks)
     def theTest(part: String) = if (startTest(part)) Some(part) else None
@@ -18,9 +28,12 @@ object FlowGroup {
   }
 
   /**
-   * The generic function that groups strings into larger something else
+   * The generic function that groups strings into larger a something else
+   * @input the flow of strings to process
+   * @param startTest function that returns something if and only if the given string marks the beginning of a string block
+   * @param build function that build a new output item made of the start entry given by "startTest" and the following strings
+   * @return the new flow of something else
    */
-
   def reassemble[F, R](
     input: Stream[String],
     startTest: String => Option[F],
