5a30f29f1680fedd1cd76900b257a4ae4540984a
david crosson <david.crosson@orange.com>
2019-11-02 20:03:35 +0100

    Enhance csv row cells splitter algorithm and add many more test cases

--- src/main/scala/fr/janalyse/split/CsvSplit.scala
+++ src/main/scala/fr/janalyse/split/CsvSplit.scala
@@ -13,18 +13,21 @@ object CsvSplit {
    * @return vector of substrings
    */
   final def split(line: String, sep: Char = ',', maxcount: Int = 0): Vector[String] = {
-    @tailrec def worker(me: String, accumulator: Vector[String] = Vector.empty): Vector[String] = {
-      if (me.isEmpty) accumulator
-      else {
+    def isSpace(ch: Char): Boolean = ch.isSpaceChar || ch == '\r' || ch == '\t'
+    @tailrec
+    def worker(me: String, accumulator: Vector[String] = Vector.empty, hasNext:Boolean=false): Vector[String] = {
+      if (me.isEmpty) {
+        if (hasNext) accumulator:+"" else accumulator
+      } else {
         val ch = me.head
-        if (ch.isSpaceChar || ch == sep) worker(me.tail, accumulator)
+        if (isSpace(ch)) worker(me.tail, accumulator, hasNext)
         else if (maxcount > 0 && accumulator.size + 1 == maxcount) accumulator :+ me.trim
         else if (ch == '"' || ch == '\'') {
           val (left, right) = me.tail.span(_ != ch)
-          worker(right.tail, accumulator :+ left)
+          worker(right.tail.trim.drop(1), accumulator :+ left)
         } else {
           val (left, right) = me.span(_ != sep)
-          worker(right, accumulator :+ left.trim)
+          worker(right.trim.drop(1), accumulator :+ left.trim, !right.isEmpty)
         }
       }
     }
--- src/test/scala/fr/janalyse/split/CsvSplitTest.scala
+++ src/test/scala/fr/janalyse/split/CsvSplitTest.scala
@@ -1,27 +1,62 @@
 package fr.janalyse.split
 
-import org.scalatest.FunSuite
+import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
-class CsvSplitTest extends FunSuite {
+class CsvSplitTest extends FlatSpec {
   import CsvSplit._
 
-  test("Direct csv split tests") {
+  "csv cell split" should "support empty cell" in {
     split("") should equal(Vector())
     split("   ") should equal(Vector())
-    split("a") should equal(Vector("a"))
-    split("   a   ") should equal(Vector("a"))
-    split("""a, b, c""") should equal(Vector("a", "b", "c"))
-    split(""" a, b,  c """) should equal(Vector("a", "b", "c"))
-    split("""a, 'b c', d""") should equal(Vector("a", "b c", "d"))
-    split("""a, "b c", d""") should equal(Vector("a", "b c", "d"))
-    split("""a ,"b, c" ,d""") should equal(Vector("a", "b, c", "d"))
+    split(" \t\r ") should equal(Vector())
   }
 
-  test("extends csv split tests") {
+  it should "support single cell" in {
+    split("a") should equal(Vector("a"))
+    split("   a   ") should equal(Vector("a"))
+    split("abcdef") should equal(Vector("abcdef"))
+  }
+
+  it should "be able to split multiple simple content cells" in {
+    split("""a, b, c""") should equal(Vector("a", "b", "c"))
+    split(""" a, b,  c """) should equal(Vector("a", "b", "c"))
+    split("""a, b c, d""") should equal(Vector("a", "b c", "d"))
+    split("""a,  b   c , d""") should equal(Vector("a", "b   c", "d"))
+  }
+
+  it should "allow empty cells" in {
+    split(",a") should equal(Vector("", "a"))
+    split("a,") should equal(Vector("a", ""))
+    split(",a,") should equal(Vector("", "a", ""))
+    split("  ,  a") should equal(Vector("", "a"))
+    split("  ,  a,,") should equal(Vector("", "a", "", ""))
+  }
+
+  it should "allow any kind of content in cells even cells separator when double or single quotes are used" in {
+    split("""a,'b c', d""") should equal(Vector("a", "b c", "d"))
+    split("""a,  'b c'  , d""") should equal(Vector("a", "b c", "d"))
+    split("""a, "b c", d""") should equal(Vector("a", "b c", "d"))
+    split("""a ,"b, c" ,d""") should equal(Vector("a", "b, c", "d"))
+    split("""a ," b,  c " ,d""") should equal(Vector("a", " b,  c ", "d"))
+  }
+
+  it should "allow to customize a maximumun number of splitted cells, remaining others unchanged" in {
     split("1, 2, 3") should equal(Vector("1", "2", "3"))
+    split("1, 2, 3", maxcount = -1) should equal(Vector("1", "2", "3"))
+    split("1, 2, 3", maxcount = 0) should equal(Vector("1", "2", "3"))
     split("1, 2, 3", maxcount = 1) should equal(Vector("1, 2, 3"))
     split("1, 2, 3", maxcount = 2) should equal(Vector("1", "2, 3"))
     split("1, 2, 3", maxcount = 3) should equal(Vector("1", "2", "3"))
+  }
+
+  it should "support custom cell separator" in {
+    split(" 0,1; 0,2 ; 3", sep=';') should equal(Vector("0,1", "0,2", "3"))
+  }
+
+  it should "manage border line cases" in {
+    split("truc, 'machin") should equal(Vector("truc", "machin"))
+    split("""truc, "'machin""") should equal(Vector("truc", "'machin"))
+    split("1, l'espoir, 2") should equal(Vector("1", "l'espoir", "2"))
   }
 }
